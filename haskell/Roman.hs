80abd1f61b4010175a21f01955faf92916b1444e
Daniel Brice <danielbrice@gmail.com>
2016-03-17 16:20:40 -0700

    this is a 4 kyu cata?

---
+++ haskell/Roman.hs
@@ -0,0 +1,44 @@
+-- import Data.List (foldl')
+
+
+solution :: String -> Int
+solution string = helper 0 string
+  where
+    helper acc "" = acc
+    helper acc (x : "") = acc + charMap x
+    helper acc (x : y : rest) =
+      case (x, y) of
+        ('I', 'V') -> helper (acc + 4) rest
+        ('I', 'X') -> helper (acc + 9) rest
+        ('X', 'L') -> helper (acc + 40) rest
+        ('X', 'C') -> helper (acc + 90) rest
+        ('C', 'D') -> helper (acc + 400) rest
+        ('C', 'M') -> helper (acc + 900) rest
+        _ -> helper (acc + charMap x) (y : rest)
+
+
+charMap :: Char -> Int
+charMap char = case char of
+  'I' -> 1
+  'V' -> 5
+  'X' -> 10
+  'L' -> 50
+  'C' -> 100
+  'D' -> 500
+  'M' -> 1000
+
+
+main :: IO ()
+main = print (test solution solutionTests)
+
+
+test :: Eq b => (a -> b) -> [(a, b)] -> [Bool]
+test f = map (\(x, y) -> f x == y)
+
+
+solutionTests :: [(String, Int)]
+solutionTests =
+  [ ("XXI", 21)
+  , ("I", 1)
+  , ("IV", 4)
+  ]
